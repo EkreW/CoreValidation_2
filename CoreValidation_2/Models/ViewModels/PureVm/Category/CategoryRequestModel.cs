37375aecd8fef5de2b0e76ccae7868627a117166
Ekrem Güngör <ekrmdsgnr@gmail.com>
2024-03-18 21:49:52 +0300

    Add project files.

---
+++ CoreValidation_2/Models/ViewModels/PureVm/Category/CategoryRequestModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreValidation_2.Models.ViewModels.PureVm.Category
+{
+    //PureVM
+    //CategoryVM
+    //CategoryRequestModel
+    //CategoryResponseModel
+    public class CategoryRequestModel
+    {
+        //DataAnnotation'da yer tutucu operatorleri sayılarla verilir...0, ilgili property neyse direkt o ismi alır...
+
+        [Required(ErrorMessage = "{0} zorunlu bir alandır.")]
+        [Display(Name = "Kategori ismi")] //Bizim property'miz kullanıcıya istediğimiz şekilde gösteriyoruz
+        [MinLength(5, ErrorMessage = "{0} an az {1} karakter alabilir")]
+        [MaxLength(15, ErrorMessage = "{0} en fazla {1} karakter alabilir")]
+        public string CategoryName { get; set; }
+
+        [Required(ErrorMessage = "{0} zorunlu bir alandır")]
+        [Display(Name = "Aciklama")]
+        [MinLength(15, ErrorMessage = "{0} an az {1} karakter alabilir")]
+        [MaxLength(100, ErrorMessage = "{0} en fazla {1} karakter alabilir")]
+        public string Description { get; set; }
+
+    }
+}
